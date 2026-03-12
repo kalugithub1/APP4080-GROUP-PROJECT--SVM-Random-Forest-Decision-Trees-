3eb51edd89d509f0c91056674a09b120842d2ccf
Mark <mgmunene@usiu.ac.ke>
2026-03-12 18:49:03 +0300

    Remove redundant files (code now integrated into main notebook)

--- random_forest_model.py
+++
@@ -1,3 +0,0 @@
-model_f = RandomForestClassifier()
-model_l = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=0)
-classifiermodel(model_f,model_l,xf_train, xf_test,yf_train,yf_test,xl_train, xl_test,yl_train,yl_test)
